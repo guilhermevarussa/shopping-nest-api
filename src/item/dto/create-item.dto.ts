d0414e37e1490d62519da5d9e4252aa5e4a1924b
guilhermevarussa <guilhermevarussa@gmail.com>
2024-10-03 22:48:26 -0400

    Criada doc swagger no endereço /api

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator'
 
 export class CreateItemDto {
+  @ApiProperty({ example: 'Boné' })  
   @IsString()
   @IsNotEmpty()
   name: string
 
+  @ApiPropertyOptional({ example: 'Boné aba reta', description: 'Optional description of the item' })
   @IsOptional()
   @IsString()
   description: string
 
+  @ApiProperty({ example: 5, description: 'Needed quantity' })
   @IsInt()
   @Min(0)
   quantity: number
